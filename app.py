3aa31ee85ca658767dfc30b9358a75349566d40e
Filippov Denis <l.filippov.l@mail.ru>
2025-11-07 14:04:11 +0700

    Вывод цветка

--- app.py
+++ app.py
@@ -299,3 +299,12 @@ def a():
 @app.route ('/lab2/a/')
 def a2():
     return 'со слешем'
+
+flower_list=('Роза','Тюльпан','Незабудка','Ромашка')
+
+@app.route ('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+    if flower_id>=len(flower_list):
+        abort(404)
+    else:
+        return "Цветок: " + flower_list[flower_id]
