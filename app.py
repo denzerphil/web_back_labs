4db5e6b2848981980fa19814f6979f244779b6b8
Filippov Denis <l.filippov.l@mail.ru>
2025-11-25 16:03:09 +0700

    Цикл в шаблоне

--- app.py
+++ app.py
@@ -330,12 +330,14 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    name = 'Филиппов Денис'
-    lab_num = 2
-    group = 'ФБИ-33'
-    course = 3
-    return render_template('example.html', 
-                         name=name, 
-                         lab_num=lab_num, 
-                         group=group, 
-                         course=course)
+    name, lab_num, group, course = 'Филиппов Денис', 2, 'ФБИ-33', 3
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321}
+    ]
+    return render_template('example.html',
+                         name=name, lab_num=lab_num, group=group,
+                         course=course, fruits=fruits)
